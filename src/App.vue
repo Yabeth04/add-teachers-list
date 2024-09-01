73e724e205aabafda1d53da9d6f46c0bee70d21b
Yabeth <soteloyabeth@gmail.com>
2024-09-01 15:25:31 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style scoped>
+</style>
